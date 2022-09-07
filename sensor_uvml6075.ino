a888942137146a3677b0aed5ae6ccdeb16b73b9e
Nilam Latifatun Nafiza <112671627+nafizanilam@users.noreply.github.com>
2022-09-07 09:51:45 +0700

    Add files via upload

---
+++ sensor_uvml6075.ino
@@ -0,0 +1,59 @@
+#define BLYNK_TEMPLATE_ID "TMPLOpF3HKye"
+#define BLYNK_DEVICE_NAME "Chart UV"
+#define BLYNK_AUTH_TOKEN "ZHX4hs9H2pl527tjn__RGauRxmZLvuJZ"
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+#include <SparkFun_VEML6075_Arduino_Library.h>
+VEML6075 uv;
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "KMTek";
+char pass[] = "KMTek123";
+
+BlynkTimer timer;
+
+
+void myTimerEvent()
+{
+  Blynk.virtualWrite(V5, millis() / 1000);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+
+  Wire.begin();
+
+  if (!uv.begin())
+  {
+    Serial.println("Unable to communicate with VEML6075.");
+    while (1)
+    ;
+  }
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+
+  float Uva = (uv.a());
+  float Uvb = (uv.b());
+
+  Serial.println(String(uv.a()));
+  Serial.println(String(uv.b()));
+
+  Serial.println(String(uv.a()) + ", " + String(uv.b()));
+  delay(250);
+
+  Blynk.virtualWrite(V1, Uva);
+  Blynk.virtualWrite(V2, Uvb);
+  delay(1000);
+}
